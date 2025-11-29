ee538477415f9fa4c9143f4244bca814af3cfd35
Noixo974 <nathanhebert2601@gmail.com>
2025-11-29 21:38:14 +0400

    V0.3

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -500,7 +500,7 @@ export const Hero: React.FC<HeroProps> = ({ onNavigateToServices }) => {
                               : 'bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-600'
                           }`}
                         >
-                          Bots Discord
+                          Bot Discord
                         </button>
                         <button
                           onClick={() => handleProjectTypeFilter('Autre')}
